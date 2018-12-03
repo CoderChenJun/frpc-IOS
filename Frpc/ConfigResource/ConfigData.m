b2dd5c7720c585f2671332fed048f79e96f2e6ec
rcy <renchuyi@126.com>
2018-12-03 08:55:29 +0800

    add new class ‘configData’

---
+++ Frpc/ConfigResource/ConfigData.m
@@ -0,0 +1,13 @@
+//
+//  ConfigData.m
+//  Frpc
+//
+//  Created by 任楚伊 on 2018/11/30.
+//  Copyright © 2018 yuanyl. All rights reserved.
+//
+
+#import "ConfigData.h"
+
+@implementation ConfigData
+
+@end
